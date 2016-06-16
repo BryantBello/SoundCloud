7971af2ebbf7b0b4b0e151656f9698cb433078d1
bryantbello <bryant.bello@outlook.com>
2016-06-16 00:34:27 -0400

    Stream

---
+++ sc-react-redux/components/Stream.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Stream ({ tracks = []}){
+	return (
+		<div>
+		{
+			tracks.map((track) => {
+				return <div className="track">{track.title}</div>;
+
+			})
+		}
+		</div>
+		);
+}
+
+
+export default Stream;
