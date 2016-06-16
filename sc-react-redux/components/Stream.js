ffe81be1a3f530117e7a33adec5788665c2b12c5
bryantbello <bryant.bello@outlook.com>
2016-06-16 00:40:53 -0400

    test

--- sc-react-redux/components/Stream.js
+++ sc-react-redux/components/Stream.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-function Stream ({ tracks = []}){
-	return (
-		<div>
-		{
-			tracks.map((track) => {
-				return <div className="track">{track.title}</div>;
-
-			})
-		}
-		</div>
-		);
+function Stream({ tracks = [] }) {
+  return (
+    <div>
+      {
+        tracks.map((track, key) => {
+          return <div className="track" key={key}>{track.title}</div>;
+        })
+      }
+    </div>
+  );
 }
-
 
 export default Stream;
